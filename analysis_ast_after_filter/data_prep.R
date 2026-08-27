4e2a865c8eb6b17f6d1ecb5f837fe8b9ae564cce
xtf <micic.math@gmail.com>
2026-08-27 17:05:49 +0200

    started with remapped ids

---
+++ analysis_ast_after_filter/data_prep.R
@@ -0,0 +1,174 @@
+source("./setup.R")
+source("./source_selected_ast_hc.R")
+
+#############
+# import rdpc
+#############
+
+rdcp_dat = read.csv("../dat/rdcp_exhal_analysis.csv")
+colnames(rdcp_dat)[colnames(rdcp_dat) == "measurement_id_exhal"] = "m_id"
+
+rdcp_dat = rdcp_dat[rdcp_dat$m_id %in% all_ast_hc_ids, , drop = FALSE]
+
+###########################
+# subset to relevant labels
+###########################
+
+rdcp_exhal_dat = rdcp_dat[
+  !is.na(rdcp_dat$m_id) &
+    rdcp_dat$diagnosis %in% c(
+      "HC",
+      "PUL_ASTHMA_ALLERGIC",
+      "PUL_ASTHMA_NONALLERGIC"
+      # "PUL_CF",
+      # "PUL_PCD"
+    ), ,
+  drop = FALSE
+]
+
+#################################
+# calculate FEV1 and FVC z-scores
+#################################
+
+rdcp_exhal_dat$height_exhal = rdcp_exhal_dat$height_exhal / 100
+rdcp_exhal_dat$gender = ifelse(rdcp_exhal_dat$gender == 1, 2, 1) # reset 1 to male and 2 to female
+
+rdcp_exhal_dat$fev1_z = zscore_GLIgl(
+  age = rdcp_exhal_dat$age_exhal,
+  height = rdcp_exhal_dat$height_exhal,
+  gender = rdcp_exhal_dat$gender,
+  FEV1 = rdcp_exhal_dat$fev1
+)
+rdcp_exhal_dat$fev1_z = round(rdcp_exhal_dat$fev1_z, 2)
+
+rdcp_exhal_dat$fvc_z = pctpred_GLIgl(
+  age = rdcp_exhal_dat$age_exhal,
+  height = rdcp_exhal_dat$height_exhal,
+  gender = rdcp_exhal_dat$gender,
+  FVC = rdcp_exhal_dat$fvc
+)
+rdcp_exhal_dat$fvc_z = round(rdcp_exhal_dat$fvc_z, 2)
+
+###############################################
+# import eNose dat and filter by measurement id
+###############################################
+
+e_dat_0 = read.csv("../dat/dat_ch_enose_20260105.csv")
+e_dat_1 = read.csv("../dat/dat_ch_enose_20260808.csv")
+
+colnames(e_dat_0)[colnames(e_dat_0) == "id"] = "m_id"
+colnames(e_dat_1)[colnames(e_dat_1) == "id"] = "m_id"
+
+e_dat_0 = e_dat_0[e_dat_0$m_id %in% rdcp_exhal_dat$m_id, ]
+e_dat_1 = e_dat_1[e_dat_1$m_id %in% rdcp_exhal_dat$m_id, ]
+
+e_dat_0$batch = "batch_20260105"
+e_dat_1$batch = "batch_20260808"
+
+########################
+# merge with REDCap data
+########################
+
+e_dat = rbind(e_dat_0, e_dat_1)
+
+final_ast_hc_cf_dat = merge(
+  e_dat,
+  rdcp_exhal_dat,
+  by = "m_id",
+  sort = FALSE
+)
+
+#########################
+# subset to relevant cols
+#########################
+
+sensors = c(
+  "S1",
+  "S3",
+  "S4",
+  "S5",
+  "S6",
+  "S7",
+  "S1BH",
+  "S2BH",
+  "S3BH",
+  "S4BH",
+  "S5BH",
+  "S6BH",
+  "S7BH"
+)
+
+cols = c(
+  "patient_id",
+  "m_id",
+  "diagnosis",
+  "asthma_source",
+  "diagnosis_status",
+  sensors,
+  "visit_exhal",
+  "metacholine_test_result",
+  "fev1_z",
+  "fvc_z",
+  "batch"
+)
+
+final_ast_hc_cf_dat = final_ast_hc_cf_dat[, cols]
+
+###################
+# relabel diagnosis
+###################
+
+final_ast_hc_cf_dat$diagnosis_simple = ifelse(
+  final_ast_hc_cf_dat$diagnosis == "PUL_ASTHMA_ALLERGIC",
+  "AST",
+  ifelse(final_ast_hc_cf_dat$diagnosis == "PUL_ASTHMA_NONALLERGIC",
+    "AST", "HC"
+  )
+)
+
+##########################################
+# relabel metacholine provocation response
+##########################################
+
+final_ast_hc_cf_dat$metacholine_response = ifelse(
+  (grepl("AST", final_ast_hc_cf_dat$diagnosis)) & (final_ast_hc_cf_dat$metacholine_test_result == 0),
+  "Neg", ifelse(
+    (grepl("AST", final_ast_hc_cf_dat$diagnosis)) & (final_ast_hc_cf_dat$metacholine_test_result == 1),
+    "Low", ifelse(
+      (grepl("AST", final_ast_hc_cf_dat$diagnosis)) & (final_ast_hc_cf_dat$metacholine_test_result == 2),
+      "Mid", ifelse(
+        (grepl("AST", final_ast_hc_cf_dat$diagnosis)) & (final_ast_hc_cf_dat$metacholine_test_result == 3),
+        "High", "No_test"
+      )
+    )
+  )
+)
+
+final_ast_hc_cf_dat$metacholine_response = ifelse(
+  is.na(final_ast_hc_cf_dat$metacholine_response),
+  "No_test",
+  final_ast_hc_cf_dat$metacholine_response
+)
+
+final_ast_hc_cf_dat$metacholine_response = factor(
+  final_ast_hc_cf_dat$metacholine_response,
+  levels = c("No_test", "Neg", "Low", "Mid", "High")
+)
+
+###############################################
+# diagnosis confirmed filter and only 1st visit
+###############################################
+
+diag_confirmed_mask = !is.na(final_ast_hc_cf_dat$diagnosis_status) & (final_ast_hc_cf_dat$diagnosis_status %in% c(0, 1))
+# diag_confirmed_mask = !is.na(final_ast_hc_cf_dat$diagnosis_status) & final_ast_hc_cf_dat$diagnosis_status == 1
+
+hc_mask = final_ast_hc_cf_dat$diagnosis == "HC"
+
+first_visit_mask = !is.na(final_ast_hc_cf_dat$visit_exhal) &
+  final_ast_hc_cf_dat$visit_exhal == 1
+
+final_ast_hc_cf_dat = final_ast_hc_cf_dat[
+  (diag_confirmed_mask | hc_mask) & first_visit_mask,
+  ,
+  drop = FALSE
+]
---
+++ analysis_ast_after_filter/univariate_comparison.R
@@ -0,0 +1,447 @@
+source("./data_prep.R")
+
+dat = final_ast_hc_cf_dat
+
+###############################################################
+# restrict to mid and high metacholine response only for asthma
+###############################################################
+
+table(dat$diagnosis_simple, dat$batch)
+
+hc_tests = sapply(sensors, function(s) {
+  wilcox.test(dat[[s]][dat$diagnosis_simple == "HC" & dat$batch == "batch_20260105"],
+              dat[[s]][dat$diagnosis_simple == "HC" & dat$batch == "batch_20260808"])$p.value
+})
+
+hc_dat = dat[dat$diagnosis_simple == "HC", ]
+
+hc_p_adj = p.adjust(hc_tests, method = "fdr")
+names(hc_p_adj) = sensors
+
+hc_bps = lapply(
+  sensors,
+  function(x) {
+    pwc = data.frame(
+      group1 = "batch_20260105",
+      group2 = "batch_20260808",
+      y.position = max(hc_dat[[x]], na.rm = TRUE) * 1.1,
+      p.adj = as.numeric(hc_p_adj[x]),
+      p.adj.str = p_rnd(hc_p_adj[x])
+    )
+    bp = ggboxplot(hc_dat,
+      x = "batch", y = x,
+      width = 0.5,
+      fill = "white",
+      linewidth = 0.4,
+      alpha = 0.1, legend = "none", outlier.shape = NA
+    ) +
+      geom_point(
+        data = hc_dat, aes(x = batch, y = !!sym(x), fill = batch, color = batch),
+        size = 2.4,
+        alpha = 0.5,
+        position = position_jitter(w = 0.2)
+      ) +
+      scale_color_manual(values = c("#228833", "#AA3377"), name = NULL) +
+scale_fill_manual(values = c("#228833", "#AA3377"), name = NULL) +
+      stat_pvalue_manual(
+        pwc,
+        label = "p.adj.str",
+        tip.length = 0.01,
+        label.size = 3.5,
+        hide.ns = TRUE
+      ) +
+      scale_y_continuous(expand = expansion(mult = c(0.05, 0.1))) +
+      labs(subtitle = paste0("Sensor: ", x)) +
+      xlab("") +
+      ylab("Intensity [a.u.]") +
+      theme_classic() +
+      ggtheme_no_legend
+    bp
+  }
+)
+names(hc_bps) = sensors
+
+plt_hc_bps = plot_grid(plotlist = hc_bps, ncol = 4, align = "hv")
+plt_hc_bps
+
+##############
+# pca hc batch
+##############
+
+X_pca = prcomp(
+  as.matrix(hc_dat[, sensors]),
+  center = TRUE,
+  scale. = TRUE
+)
+
+pca_importance = as.data.frame(summary(X_pca)$importance)
+
+pca_dat = data.frame(
+  X_pca$x,
+  m_id = hc_dat$m_id,
+  batch = hc_dat$batch
+)
+
+pc1_lab = paste0("PC1 ", round(pca_importance$PC1[2] * 100, 1), "%")
+pc2_lab = paste0("PC2 ", round(pca_importance$PC2[2] * 100, 1), "%")
+pc3_lab = paste0("PC3 ", round(pca_importance$PC3[2] * 100, 1), "%")
+pc4_lab = paste0("PC4 ", round(pca_importance$PC4[2] * 100, 1), "%")
+
+# diagnosis_colors = c(
+#   HC  = "#4477AA",
+#   AST = "#CC6677",
+#   AST_AL = "#CC6677",
+#   AST_NOAL = "#DDCC77"
+# )
+
+pca_by_batch_1 = ggplot(
+  pca_dat,
+  aes(x = PC1, y = PC2, color = batch)
+) +
+  geom_hline(yintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  geom_vline(xintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  stat_ellipse(aes(group = batch), show.legend = FALSE, linewidth = 0.5) +
+  geom_point(size = 2.4, alpha = 0.75) +
+  # scale_color_manual(values = diagnosis_colors) +
+  labs(
+    title = "PCA scores by batch",
+    x = pc1_lab,
+    y = pc2_lab,
+    color = "Batch"
+  ) +
+  theme_classic() +
+  ggtheme_top_right
+
+pca_by_batch_2 = ggplot(
+  pca_dat,
+  aes(x = PC1, y = PC3, color = batch)
+) +
+  geom_hline(yintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  geom_vline(xintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  stat_ellipse(aes(group = batch), show.legend = FALSE, linewidth = 0.5) +
+  geom_point(size = 2.4, alpha = 0.75) +
+  # scale_color_manual(values = diagnosis_colors) +
+  labs(
+    title = "PCA scores by batch",
+    x = pc1_lab,
+    y = pc3_lab,
+    color = "Batch"
+  ) +
+  theme_classic() +
+  ggtheme_top_right
+
+pca_by_batch_2b = ggplot(
+  pca_dat,
+  aes(x = PC2, y = PC3, color = batch)
+) +
+  geom_hline(yintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  geom_vline(xintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  stat_ellipse(aes(group = batch), show.legend = FALSE, linewidth = 0.5) +
+  geom_point(size = 2.4, alpha = 0.75) +
+  # scale_color_manual(values = diagnosis_colors) +
+  labs(
+    title = "PCA scores by batch",
+    x = pc2_lab,
+    y = pc3_lab,
+    color = "Batch"
+  ) +
+  theme_classic() +
+  ggtheme_top_right
+
+pca_by_batch_3 = ggplot(
+  pca_dat,
+  aes(x = PC1, y = PC4, color = batch)
+) +
+  geom_hline(yintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  geom_vline(xintercept = 0, linewidth = 0.4, color = "grey70", linetype = "dashed") +
+  stat_ellipse(aes(group = batch), show.legend = FALSE, linewidth = 0.5) +
+  geom_point(size = 2.4, alpha = 0.75) +
+  # scale_color_manual(values = diagnosis_colors) +
+  labs(
+    title = "PCA scores by batch",
+    x = pc1_lab,
+    y = pc4_lab,
+    color = "Batch"
+  ) +
+  theme_classic() +
+  ggtheme_top_right
+
+plt_pca_hc = cowplot::plot_grid(
+  pca_by_batch_1,
+  pca_by_batch_2,
+  pca_by_batch_2b,
+  pca_by_batch_3,
+  nrow = 2,
+  align = "hv"
+)
+plt_pca_hc
+
+#####################
+# restrict to batch 2
+#####################
+
+# dat = dat[dat$batch == "batch_20260808", , drop = FALSE]
+
+#########################
+# remove S2 normalization
+#########################
+
+# X = as.matrix(dat[, sensors])
+# X_clr = log(X) - rowMeans(log(X))
+# 
+# dat[, sensors] = X_clr
+
+####
+# st
+####
+
+w_test = mclapply(
+  sensors,
+  function(x) {
+    ast = dat[dat$diagnosis_simple == "AST", x]
+    hc = dat[dat$diagnosis_simple == "HC", x]
+    w = wilcox.test(ast, hc, conf.int = TRUE)
+    d = data.frame(
+      sensor = x,
+      diff_cf_vs_hc_ci = paste0(
+        n_rnd(w$estimate, 2),
+        " (",
+        n_rnd(w$conf.int[[1]], 2),
+        ", ",
+        n_rnd(w$conf.int[[2]], 2),
+        ")"
+      ),
+      p_val = w$p.value
+    )
+    rownames(d) = NULL
+    d
+  },
+  mc.cores = parallel::detectCores() - 2
+)
+w_test = do.call(rbind, w_test)
+
+p_adj = p.adjust(w_test$p_val, method = "fdr")
+names(p_adj) = sensors
+w_test$p_adj = p_adj
+
+p_dat_format = w_test
+p_dat_format$p_val = p_rnd(p_dat_format$p_val)
+p_dat_format$p_adj = p_rnd(p_dat_format$p_adj)
+
+# p_dat_format[
+#   ,
+#   c(
+#     "sensor",
+#     "diff_cf_vs_hc_ci_base",
+#     "p_adj_base",
+#     "diff_cf_vs_hc_ci_end",
+#     "p_adj_end"
+#   )
+# ]
+
+colnames(p_dat_format) = c(
+  "Sensor",
+  "Est. difference (95% CI)",
+  "Raw p-value",
+  "Adjusted p-value"
+)
+
+p_dat_format
+
+# write.csv(p_dat_format, "./tables/univariate_results.csv", row.names = FALSE)
+
+####
+# bp
+####
+
+bps = lapply(
+  sensors,
+  function(x) {
+    pwc = data.frame(
+      group1 = "AST",
+      group2 = "HC",
+      y.position = max(dat[[x]], na.rm = TRUE) * 1.1,
+      p.adj = as.numeric(p_adj[x]),
+      p.adj.str = p_rnd(p_adj[x])
+    )
+    bp = ggboxplot(dat,
+      x = "diagnosis_simple", y = x,
+      width = 0.5,
+      fill = "white",
+      linewidth = 0.4,
+      alpha = 0.1, legend = "none", outlier.shape = NA
+    ) +
+      geom_point(
+        data = dat, aes(x = diagnosis_simple, y = !!sym(x), fill = diagnosis_simple, color = diagnosis_simple),
+        size = 2.4,
+        alpha = 0.5,
+        position = position_jitter(w = 0.2)
+      ) +
+      scale_color_manual(
+        # values = c("#29ABE2", "#D9534F", "#A1A79E", "#F2D75C"),
+        values = c("#CC6677", "#4477AA"),
+        name = NULL
+      ) +
+      scale_fill_manual(
+        # values = c("#29ABE2", "#D9534F", "#A1A79E", "#F2D75C"),
+        values = c("#CC6677", "#4477AA"),
+        name = NULL
+      ) +
+      stat_pvalue_manual(
+        pwc,
+        label = "p.adj.str",
+        tip.length = 0.01,
+        label.size = 3.5,
+        hide.ns = TRUE
+      ) +
+      scale_y_continuous(
+        expand = expansion(mult = c(0.05, 0.1))
+      ) +
+      # # scale_x_discrete(labels = labels_xaxis, expand = c(0.5, 0.5)) +
+      # ylim(min(dat[[sensor]]) - 0.1 * min(dat[[sensor]]), max(pwc$y.position) + 0.1 * max(pwc$y.position)) +
+      labs(subtitle = paste0("Sensor: ", x)) +
+      xlab("") +
+      ylab("Intensity [a.u.]") +
+      theme_classic() +
+      ggtheme_no_legend
+    bp
+  }
+)
+names(bps) = sensors
+
+plt_bps = plot_grid(
+  plotlist = bps,
+  ncol = 4,
+  align = "hv"
+)
+plt_bps
+
+# ggsave("./plots/ast_hc_s3.png", bps$S3, width = 10, height = 10, units = "cm", dpi = 400, bg = "white")
+# ggsave("./plots/ast_hc_s7.png", bps$S7, width = 10, height = 10, units = "cm", dpi = 400, bg = "white")
+
+#############
+# spider plot
+#############
+
+centers = aggregate(
+  cbind(S3, S7) ~ diagnosis_simple,
+  data = dat,
+  FUN = mean
+)
+
+dat_s3_s7 = merge(
+  dat,
+  centers,
+  by = "diagnosis_simple",
+  suffixes = c("", "_center")
+)
+
+plt_s3_s7 = ggplot(dat_s3_s7, aes(x = S3, y = S7, color = diagnosis_simple)) +
+  geom_segment(
+    aes(
+      x = S3_center,
+      y = S7_center,
+      xend = S3,
+      yend = S7,
+      color = diagnosis_simple
+    ),
+    alpha = 0.25,
+    linewidth = 0.4
+  ) +
+  geom_point(size = 2.4, alpha = 0.65) +
+  geom_point(
+    aes(x = S3_center, y = S7_center, fill = diagnosis_simple),
+    shape = 21,
+    size = 1,
+    # color = "black",
+    stroke = 0.5
+  ) +
+  scale_color_manual(values = c(AST = "#CC6677", HC = "#4477AA"), labels = c(AST = "Asthma", HC = "Healthy")) +
+  scale_fill_manual(values = c(AST = "#CC6677", HC = "#4477AA"), labels = c(AST = "Asthma", HC = "Healthy")) +
+  labs(
+    x = "S3 intensity [a.u.]",
+    y = "S7 intensity [a.u.]"
+  ) +
+  theme_classic() +
+  ggtheme_top_right
+# plt_s3_s7
+
+# ggsave("./plots/spider_plot_s3_s7.png", plt_s3_s7, width = 10, height = 10, units = "cm", dpi = 400, bg = "white")
+
+######
+# test
+######
+
+# dat$S3_S7_logratio = log(dat$S3 / dat$S7)
+# 
+# aggregate(
+#   S3_S7_logratio ~ diagnosis_simple,
+#   data = dat,
+#   FUN = function(x) c(
+#     mean = mean(x),
+#     sd = sd(x),
+#     IQR = IQR(x)
+#   )
+# )
+# 
+# ggboxplot(
+#   dat,
+#   x = "diagnosis_simple",
+#   y = "S3_S7_logratio",
+#   add = "jitter"
+# )
+# 
+# intensity_sensors = c("S1", "S3", "S4", "S5", "S6", "S7")
+# 
+# pairs = combn(intensity_sensors, 2, simplify = FALSE)
+# 
+# ratio_variability = do.call(
+#   rbind,
+#   lapply(pairs, function(z) {
+# 
+#     lr = log(dat[[z[1]]] / dat[[z[2]]])
+# 
+#     data.frame(
+#       pair = paste(z, collapse = "/"),
+#       sd_HC = sd(lr[dat$diagnosis_simple == "HC"]),
+#       sd_AST = sd(lr[dat$diagnosis_simple == "AST"])
+#     )
+#   })
+# )
+# 
+# ratio_variability
+
+# plot_grid(
+#   plt_hc_bps, plt_bps
+# )
+
+jitter_pos = position_jitter(
+  width = 0.15,
+  height = 0,
+  seed = 123
+)
+
+temp_plt_s7 = ggplot(dat, aes(x = diagnosis_simple, y = S7)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_point(position = jitter_pos) +
+  geom_text_repel(
+    aes(label = m_id),
+    position = jitter_pos,
+    max.overlap = Inf
+  ) +
+  labs(title = "S7") +
+  theme_classic()
+temp_plt_s3 = ggplot(dat, aes(x = diagnosis_simple, y = S3)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_point(position = jitter_pos) +
+  geom_text_repel(
+    aes(label = m_id),
+    position = jitter_pos,
+    max.overlap = Inf
+  ) +
+  labs(title = "S3") +
+  theme_classic()
+
+plot_grid(
+  temp_plt_s7, temp_plt_s3
+)
